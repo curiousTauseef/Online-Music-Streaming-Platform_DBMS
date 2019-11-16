4d2bedf7b79a4233d786d61105190a5b0a564b29
Md. Musfiqur Rahaman <musfiqur.rahaman@northsouth.edu>
2019-11-16 23:30:43 +0600

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,27 @@
+<?php
+
+	include("includes/config.php");
+	//session_destroy(); for logout
+
+	if (isset($_SESSION['userLoggedIn'])) {
+		$userLoggedIn = $_SESSION['userLoggedIn'];
+	}
+	else{
+		header("Location: register.php");
+	}
+
+ ?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<title>Welcome to OMSP!</title>
+	<title>OMSP || Home </title>
 </head>
 <body>
-	Bismillahir Rahmanir Rahim
+	<center>
+		<h1>Welcome <br>to<br> Online Music Streaming Platform</h1>
+	</center>
 
 </body>
 </html>
