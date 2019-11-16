7ee62729c6bdea5ca7c6e0a0a601dac599b4d62b
Md. Musfiqur Rahaman <musfiqur.rahaman@northsouth.edu>
2019-11-16 23:24:31 +0600

    Update config.html

--- includes/config.php
+++ includes/config.php
@@ -1,11 +1,13 @@
 <?php 
 	
 	ob_start();
+	session_start();
+
 	$timezone = date_default_timezone_set("Asia/Dhaka");
 	$con = mysqli_connect("localhost", "root", "", "omsp");
 
 	if (mysqli_connect_errno()){
-		echo "Failed to connect" .mysqli_connect_errno();
+		echo "Failed to connect:" . mysqli_connect_errno();
 	}
 
 
